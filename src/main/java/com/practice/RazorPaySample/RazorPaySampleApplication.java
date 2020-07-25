f7ae68c2ef2f104dd7b3717e767516888d64a5e2
amit.chaurasia <amit.chaurasia@snapdeal.com>
2020-07-25 18:13:39 +0530

    initial commit

---
+++ src/main/java/com/practice/RazorPaySample/RazorPaySampleApplication.java
@@ -0,0 +1,13 @@
+package com.practice.RazorPaySample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazorPaySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RazorPaySampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/RazorPaySample/RazorPaySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.RazorPaySample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazorPaySampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
